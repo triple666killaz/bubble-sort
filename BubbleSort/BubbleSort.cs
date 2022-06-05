489505f812ff33cde0434b24e46583552e7b9df3
triple666killaz <blackrascal14@gmail.com>
2022-06-05 20:09:08 +0300

    First commit

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if(arr[i] < arr[j])
+                    {
+                        temp =arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-10, 10);
+            }
+                
+            BubbleSort.Sort(arr);
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
